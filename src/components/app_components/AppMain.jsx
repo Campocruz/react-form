08e00288589d059efb3a5744bf6386abab7865b0
Campocruz <cosmacampo93@yahoo.it>
2026-05-02 10:23:42 +0200

    add botton to remove item from list

--- src/components/app_components/AppMain.jsx
+++ src/components/app_components/AppMain.jsx
@@ -21,27 +21,32 @@ export default function AppMain() {
     const newObj = { id: Date.now(), title: newArticle }
     setArticles([newObj, ...articles])
     setNewArticle('')
-    console.log(articles);
+  }
 
+  function deleteItem(i) {
+    const filterdIndex = articles.filter((article, index) => index !== i)
+    console.log(filterdIndex);
+    setArticles(filterdIndex);
   }
 
   return (
     <>
       <div className="container">
         <h2>Article List</h2>
-        {
-          articles.map((item, i) => (
-            <div key={i} className="card">
-              <div className="card-heater">
-                <h3>{item.title}</h3>
-              </div>
-            </div>))
-        }
         <form onSubmit={handleSubmit}>
           <input type="text" value={newArticle} onChange={e => setNewArticle(e.target.value)} />
           <button>Add New Article</button>
         </form>
       </div>
+      {
+        articles.map((item, i) => (
+          <div key={i} className="card">
+            <div className="card-heater">
+              <h3>{item.title}</h3>
+            </div>
+            <button className="btn btn-dark" onClick={() => deleteItem(i)}><i className="bi bi-trash"></i></button>
+          </div>))
+      }
     </>
   )
 }
